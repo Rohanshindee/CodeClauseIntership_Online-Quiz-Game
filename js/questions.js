e1f347a2487355afa4179451ed762a09193004cb
Shinde Rohan Prabhakar <149306560+Rohanshindee@users.noreply.github.com>
2025-01-22 14:05:07 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,223 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is the capital of Maharashtra?",
+    answer: "Mumbai",
+    options: [
+      " Nagpur",
+      "Satara ",
+      "Pune ",
+      "Mumbai"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Who is the father of Computer?",
+    answer: "Charles Babbage",
+    options: [
+      "James Gosling ",
+      "Charles Babbage",
+      "Dennis Ritchin",
+      "Bjarne Stroustrup"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the full form of CPU?",
+    answer: "Central Processing Unit",
+    options: [
+      "Computer Processing unit",
+      "Computer Principle unit",
+      "Central Processing Unit",
+      "Control Processing unit "
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following language does the computer understand?",
+    answer: "Computer understands only Binary Language",
+    options: [
+      "Computer understands only C Language",
+      "Computer understands only Assembly Language",
+      "Computer understands only Binary Language",
+      "Computer understands only BASIC"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following computer language is written in binary codes only?",
+    answer: "machine language",
+    options: [
+      "JAVA",
+      "machine language",
+      "C",
+      "C#"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Which of the following is the brain of the computer?",
+    answer: "Central Processing Unit",
+    options: [
+      "Memory",
+      "Central Processing Unit",
+      "Ram",
+      "ROM"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Which of the following is not a characteristic of a computer? ?",
+    answer: "IQ",
+    options: [
+      "IQ",
+      "Versatility",
+      "Accurecy",
+      "Diligence"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Which of the following is the smallest unit of data in a computer?",
+    answer: "Bit",
+    options: [
+      "Byte",
+      "GB",
+      "Bit",
+      "KB"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Which of the following monitor looks like a television and are normally used with non-portable computer systems?",
+    answer: "CRT",
+    options: [
+      "CRT",
+      "LED",
+      "LCD",
+      "Moniter"
+    ]
+  },
+  {
+    numb: 10,
+    question: " Which of the following is not a type of computer code?",
+    answer: "EDIC",
+    options: [
+      "ASCLL",
+      "BCD",
+      "EBCDIC",
+      "EDIC"
+    ]
+  },
+  {
+    numb: 11,
+    question: "Which of the following can access the server?",
+    answer: "Web Client",
+    options: [
+      "Web client",
+      "User",
+      "Web Browser",
+      "Web Server"
+    ]
+  },
+  {
+    numb: 12,
+    question: "Which of the following is known as the language made up of binary-coded instructions?",
+    answer: "Machine",
+    options: [
+      "Machine",
+      "C",
+      "BAsic",
+      "High level"
+    ]
+  },
+  {
+    numb: 13,
+    question: "Which of the following is created when a user opens an account in the computer system?",
+    answer: "Subdirectory",
+    options: [
+      "Subdirectory",
+      "MFD",
+      "RFD",
+      "SFD"
+    ]
+  },
+  {
+    numb: 14,
+    question: "Which of the following service allows a user to log in to another computer somewhere on the Internet?",
+    answer: " Telnet",
+    options: [
+      "UseNet",
+      "email",
+      "FTP",
+      " Telnet"
+    ]
+  },
+  {
+    numb: 15,
+    question: "Which of the following is not a type of computer on the basis of operation? ?",
+    answer: "Remote",
+    options: [
+      "analog",
+      "Remote",
+      "hybrid",
+      "digtal"
+    ]
+  },
+  {
+    numb: 16,
+    question: "Which of the following is the first neural network computer?",
+    answer: "SNARC",
+    options: [
+      "AM",
+      "AN",
+      "SNARC",
+      "RFD"
+    ]
+  },
+  {
+    numb: 17,
+    question: " Which of the following is used in EBCDIC?",
+    answer: "Mainfarmes",
+    options: [
+      "Machine Code",
+      "Mainfarmes",
+      "Programming",
+      "Super Cumputer"
+    ]
+  },
+  {
+    numb: 18,
+    question: "Which of the following are physical devices of a computer?",
+    answer: "Hardwere",
+    options: [
+      "Softwere",
+      "System Softwere",
+      "hardwere",
+      "Package"
+    ]
+  },
+  {
+    numb: 19,
+    question: " Which of the following defines the assigned ordering among the characters used by the computer?",
+    answer: "Collating Sequence",
+    options: [
+      "Accumulation",
+      "Sorting",
+      "Collating Sequence",
+      "Unicode"
+    ]
+  },
+  {
+    numb: 20,
+    question: "Which of the following devices provides the communication between a computer and the outer world?",
+    answer: "I/O",
+    options: [
+      "compact",
+      "Strage",
+      "Drivers",
+      "I/O"
+    ]
+  },
+];
